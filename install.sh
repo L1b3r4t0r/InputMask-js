1e30a59b51426a0ba252180e0ca222865b9e762d
Matheus Xavier Silva <matheus_xavier-silva@live.com>
2015-04-07 16:48:08 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+# !/usr/bin/bash
+# ===============
+# (c)2015 Matheus Silva MIT license
+# Compile script
+# ===============
+# Fetch latest version
+curl -o latest.tar.gz https://github.com/matheusxaviersi/InputMask-js/archive/installer.tar.gz;
+tar -xfz latest.tar.gz;
+npm install traceur -g;
